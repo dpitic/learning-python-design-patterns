6d585f39395de0a60166e0041d1473e0b2bf47a0
dpitic <dpitic@gmail.com>
2016-07-27 07:25:40 +0800

    Update Singleton.py
    Updated __new__() method docstring.

--- Singleton/Singleton.py
+++ Singleton/Singleton.py
@@ -1,7 +1,7 @@
 class Singleton(object):
     '''Implementation of a Singleton class.'''
     def __new__(cls):
-        '''Return a new instance or the singleton instance if it exists.'''
+        '''Return a new instance on the first call and the same singleton instance thereafter.'''
         if not hasattr(cls, 'instance'):
             cls.instance = super(Singleton, cls).__new__(cls)
         return cls.instance
