cd266a6668dbf6fc87b51c6a004d82d005db3149
Dan Pitic <dpitic@gmail.com>
2016-07-28 08:34:35 +0800

    Docstring formatting 80 cols.

--- Singleton/Singleton.py
+++ Singleton/Singleton.py
@@ -1,7 +1,8 @@
 class Singleton(object):
     '''Implementation of a Singleton class.'''
     def __new__(cls):
-        '''Return a new instance on the first call and the same singleton instance thereafter.'''
+        '''Return a new instance on the first call and the same singleton
+        instance thereafter.'''
         if not hasattr(cls, 'instance'):
             cls.instance = super(Singleton, cls).__new__(cls)
         return cls.instance
